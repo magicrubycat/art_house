7e7ac4f62778edfc91a637165b977daa5991304f
PS <pls.eur3@gmail.com>
2020-05-25 18:24:21 +0200

    completed model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :artifacts, dependent: :destroy
+  has_many :bids, through: :artifacts
+  validates :address, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

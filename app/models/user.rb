e0aa3ac0e58d16b1253f2531a47ed9fd4f8d10e5
PS <pls.eur3@gmail.com>
2020-05-25 18:53:27 +0200

    updated user model with has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :artifacts, dependent: :destroy
-  has_many :bids, through: :artifacts
+  has_many :artifacts, through: :bids
+  has_many :bids, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

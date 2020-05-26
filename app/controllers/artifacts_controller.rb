01ec920d97963e4df1149ba1d0be193a0c1d3e16
j-jasmin <jasmin.jenull@gmx.at>
2020-05-26 15:18:44 +0200

    added actions new and create in artifacts controller and form new

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,5 +1,5 @@
 class ArtifactsController < ApplicationController
-  #skip_before_action :authenticate_user!, only: index, show
+  #skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_artifact, only: [:show]
 
   def index
@@ -9,20 +9,19 @@ class ArtifactsController < ApplicationController
   def show
   end
 
-=begin
   def new
     @artifact = Artifact.new
   end
 
   def create
-    @artifact = Artifact.new(artifact_params)
+    @artifact = Artifact.new(artifacts_params)
+    @artifact.user = current_user
     if @artifact.save
       redirect_to artifact_path(@artifact)
     else
       render :new
     end
   end
-=end
 
   def destroy
 
@@ -34,8 +33,8 @@ class ArtifactsController < ApplicationController
     @artifact = Artifact.find(params[:id])
   end
 
-  # def artifacts_params
-  #   params.require(:artifact).permit(:name, :artist, :year, :description, :starting_value, :sold)
-  # end
+  def artifacts_params
+    params.require(:artifact).permit(:name, :artist, :year, :description, :starting_value, :sold)
+  end
 
 end

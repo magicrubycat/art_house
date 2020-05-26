cab73bd8d8bd93afadeeb63ae74818367c8af90f
Carina Meissl <carina.meissl@hotmail.com>
2020-05-26 15:34:09 +0200

    front-end homepage

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,30 @@ ActiveRecord::Schema.define(version: 2020_05_25_163444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artifacts", force: :cascade do |t|
+    t.string "name"
+    t.string "artist"
+    t.integer "year"
+    t.text "description"
+    t.integer "starting_value"
+    t.boolean "sold"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_artifacts_on_user_id"
+  end
+
+  create_table "bids", force: :cascade do |t|
+    t.integer "value"
+    t.bigint "user_id", null: false
+    t.bigint "artifact_id", null: false
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artifact_id"], name: "index_bids_on_artifact_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +53,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_163444) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "artifacts", "users"
+  add_foreign_key "bids", "artifacts"
+  add_foreign_key "bids", "users"
 end

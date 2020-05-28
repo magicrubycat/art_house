7721b805db2dd01b60a0ee4461b25a733b090b45
PS <pls.eur3@gmail.com>
2020-05-28 14:58:46 +0200

    pundit updated with all the dependencies in controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bids # bids as a potential buyer of an artifact
-  has_many :artifacts
+  has_many :bids, dependent: :destroy # bids as a potential buyer of an artifact
+  has_many :artifacts, dependent: :destroy
   has_many :bids_as_owner, through: :artifacts, source: :bids
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+# you always want to use dependent destroy when there are has_many - never on belongs_to
+# as the belongs_to is the dependent i.e. the child of the parent has_many

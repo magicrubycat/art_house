60a4edadd3c4ac93f3e2fade95ddf084e2f4ae2a
alon kraus <alonkrauslaw@gmail.com>
2020-05-28 17:39:55 +0200

    Faker with Bids and Images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,98 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require "open-uri"
+Bid.destroy_all
+Artifact.destroy_all
+User.destroy_all
 
-emails = ["alon@art-house.com", "petronela@art-house.com", "carina@art-house.com", "cornelia@art-house.com", "jasmin@art-house.com"]
-emails.each do |email|
-   user = User.new(
-     email: email,
-     password: "123456")
-   user.save!
-   5.times do
-   artifact = Artifact.new(
-     name: Faker::Lorem.unique.word.capitalize(),
-     artist: Faker::Artist.name,
-     year: rand(1790..2020),
-     description: Faker::Lorem.paragraphs,
-     starting_value: rand(300..21500),
-     user: user
-  )
-  artifact.save!
- end
+
+        user = User.create!(
+        email: 'alon@art-house.com',
+        password: "123456"
+    )
+       user = User.create!(
+        email: 'petronela@art-house.com',
+        password: "123456"
+    )
+       user = User.create!(
+        email: 'carina@art-house.com',
+        password: "123456"
+    )
+       user = User.create!(
+        email: 'cornelia@art-house.com',
+        password: "123456"
+    )
+        file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+       
+        artifact = Artifact.create!(
+            name: Faker::Lorem.unique.word.capitalize(),
+            artist: Faker::Artist.name,
+            year: rand(1790..2020),
+            description: Faker::Lorem.paragraphs,
+            starting_value: rand(300..21500),
+            user_id: User.first.id
+        )
+         artifact.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+        file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+       
+        artifact = Artifact.create!(
+            name: Faker::Lorem.unique.word.capitalize(),
+            artist: Faker::Artist.name,
+            year: rand(1790..2020),
+            description: Faker::Lorem.paragraphs,
+            starting_value: rand(300..21500),
+            user_id: User.first.id
+        )
+         artifact.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+        file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+       
+        artifact = Artifact.create!(
+            name: Faker::Lorem.unique.word.capitalize(),
+            artist: Faker::Artist.name,
+            year: rand(1790..2020),
+            description: Faker::Lorem.paragraphs,
+            starting_value: rand(300..21500),
+            user_id: User.first.id
+        )
+         artifact.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+        file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+       
+        artifact = Artifact.create!(
+            name: Faker::Lorem.unique.word.capitalize(),
+            artist: Faker::Artist.name,
+            year: rand(1790..2020),
+            description: Faker::Lorem.paragraphs,
+            starting_value: rand(300..21500),
+            user_id: User.first.id + 1
+        )
+         artifact.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  
+         file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+       
+        artifact = Artifact.create!(
+            name: Faker::Lorem.unique.word.capitalize(),
+            artist: Faker::Artist.name,
+            year: rand(1790..2020),
+            description: Faker::Lorem.paragraphs,
+            starting_value: rand(300..21500),
+            user_id: User.first.id + 1
+        )
+         artifact.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+        
+User.all.each do |user|
+    2.times do
+        bid = Bid.new(
+            artifact: Artifact.where.not(user_id: user.id).sample,
+            user: user,
+            status: "pending",
+            value: rand(1..10000000)
+        )
+        bid.save!   
+     
+    end
+
 end
